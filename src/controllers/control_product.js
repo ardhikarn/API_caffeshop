2324cc93bc7888a2e0359d38d0c566e1a5edf188
ardhikarn <ardhikarn@gmail.com>
2020-09-04 14:46:22 +0700

    update delete image

--- src/controllers/control_product.js
+++ src/controllers/control_product.js
@@ -1,4 +1,3 @@
-const { request } = require("express");
 const {
   getProduct,
   getProductCount,
@@ -9,7 +8,9 @@ const {
 } = require("../models/model_product");
 const qs = require("querystring");
 const helper = require("../helper/helper");
-const { response } = require("../helper/helper");
+const redis = require("redis");
+const client = redis.createClient();
+const fs = require("fs");
 
 const getPrevLink = (page, currentQuery) => {
   if (page > 1) {
@@ -72,6 +73,12 @@ module.exports = {
     };
     try {
       const result = await getProduct(limit, offset, search, sort, ascDesc);
+      // proses set data result ke dalam redis
+      client.setex(
+        `getproduct:${JSON.stringify(request.query)}`,
+        3600,
+        JSON.stringify(result)
+      );
       return helper.response(
         response,
         200,
@@ -89,6 +96,7 @@ module.exports = {
       const { id } = request.params;
       const result = await getProductById(id);
       if (result.length > 0) {
+        client.setex(`getproductbyid:${id}`, 3600, JSON.stringify(result));
         return helper.response(
           response,
           200,
@@ -107,14 +115,13 @@ module.exports = {
       const {
         product_name,
         product_price,
-        product_image,
         category_id,
         product_status,
       } = request.body;
       const addData = {
         product_name,
         product_price,
-        product_image,
+        product_image: request.file === undefined ? "" : request.file.filename,
         category_id,
         product_created_at: new Date(),
         product_status,
@@ -131,27 +138,32 @@ module.exports = {
       const {
         product_name,
         product_price,
-        product_image,
         category_id,
         product_status,
       } = request.body;
       const updateData = {
         product_name,
         product_price,
-        product_image,
+        product_image: request.file === undefined ? "" : request.file.filename,
         category_id,
         product_updated_at: new Date(),
         product_status,
       };
       const checkId = await getProductById(id);
       if (checkId.length > 0) {
-        const result = await patchProduct(updateData, id);
-        return helper.response(
-          response,
-          200,
-          `Product id: ${id} Updated`,
-          result
-        );
+        fs.unlink(`./uploads/${checkId[0].product_image}`, async (error) => {
+          if (error) {
+            throw error;
+          } else {
+            const result = await patchProduct(updateData, id);
+            return helper.response(
+              response,
+              200,
+              `Product id: ${id} Updated`,
+              result
+            );
+          }
+        });
       } else {
         return helper.response(response, 404, `Product id: ${id} not Found`);
       }
@@ -162,8 +174,28 @@ module.exports = {
   deleteProduct: async (request, response) => {
     try {
       const { id } = request.params;
-      const result = await deleteProduct(id);
-      return helper.response(response, 200, `Data id: ${id} Deleted`);
+      const checkId = await getProductById(id);
+      if (checkId.length > 0) {
+        fs.unlink(`./uploads/${checkId[0].product_image}`, async (error) => {
+          if (!error) {
+            const result = await deleteProduct(id);
+            return helper.response(
+              response,
+              200,
+              `Data Product and Data Redis: id ${id} Deleted`
+            );
+          } else {
+            return helper.response(
+              response,
+              201,
+              "File doesn't exist, won't remove it.",
+              error
+            );
+          }
+        });
+      } else {
+        return helper.response(response, 200, `Data id: ${id} Deleted`);
+      }
     } catch (error) {
       return helper.response(response, 400, "Bad Request", error);
     }
